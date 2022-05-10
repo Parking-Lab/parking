697b05621b7574e6efbbf67af3cfaf2414439347
ReidDye <40401995+reid23@users.noreply.github.com>
2022-05-10 11:30:32 -0700

    started sorter class docstrings!

---
+++ Sorter.py
@@ -0,0 +1,8 @@
+class Sorter:
+    def __init__(self, students: 'list[students]'):
+        """constructor for sorter object
+
+        Args:
+            students (list[students]): a list of all students to be sorted
+        """
+        pass
