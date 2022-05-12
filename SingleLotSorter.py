0b564654676a23f9373f4e29b49b5549f8066366
ReidDye <40401995+reid23@users.noreply.github.com>
2022-05-11 23:14:28 -0700

    added support for day-specific sorts

--- SingleLotSorter.py
+++ SingleLotSorter.py
@@ -34,15 +34,16 @@ class SingleLotSorter:
         """
         pass
 
-    def __getitem__(index: 'int|str|Student' = 0):
+    def __getitem__(day: int, index: 'int|str|Student' = 0):
         """gets an item, using subscript notation like a list. Takes an int (rank), str (student's name), or Student (actual equivalent student object).
 
         Args:
+            day (int): int in [0, 4] describing the day of the week requested.
             index (int|str|Student, optional): if int, returns student in that rank (0 is best, can be negative, return Student). If str, returns rank of student with that name (int). If Student, returns rank of that Student (int). Defaults to -1.
 
         Raises:
             KeyError: if the str name or Student passed does not exist in the SingleLotSorter
-            IndexError: if the int rank passed is out of range (ie not that many students exist)
+            IndexError: if the int rank passed is out of range (ie not that many students exist) or if you entered a day that doesn't exist
             TypeError: if you try to get the student at rank 2.5
 
         Returns:
@@ -51,15 +52,18 @@ class SingleLotSorter:
 
         return
     
-    def pop():
+    def pop(day: int):
         """returns (and deletes) top ranked student, like pop function in a standard queue.
+
+        Args:
+            day (int): the day of the week you want the rankings from, in range [0, 4]
 
         Returns:
             tuple[Student, int]: tuple in the format (Student (Student object), rank (int))
         """
         pass
 
-    def delete(student: 'int|str|Student'):
+    def delete(day: int, student: 'int|str|Student'):
         """deletes the student at the rank, with the name, or equal to the Student.
 
         Raises:
@@ -68,6 +72,7 @@ class SingleLotSorter:
             TypeError: if you try to get the student at rank 2.5
 
         Args:
+            day (int): the day of the week you want the rankings to reference, in range [0, 4]
             student (int|str|Student): the student to delete. Specify rank (can be negative), name, or an identical Student object.
         """
         pass
