c4915ed1bea90a7bd3e602bcbd5fd6ad361101ae
sun-god007 <72593495+sun-god007@users.noreply.github.com>
2022-05-16 21:07:56 -0700

    student class api with some sheet stuff

--- Student.py
+++ Student.py
@@ -18,18 +18,51 @@ class Student:
     with open('distances.json', 'r') as f:
         DISTANCES = json.load(f)
     
-    def __init__(self, row):
+    def __init__(self,row,day,sheet):
         '''creates a Student object with instance variables corresponding to the student's google sheet information
 
         Args:
             row: considering Student objects might be defined in a for loop, the row variable will serve
             to access the corresponding GSheet row and access information from it
+
+            day: the day of the week the program is being run on
                    
         
         Actions:
             uses GSheet class to define instance variables (name, type of car, weights, etc.)
 
         '''
+        #do we run the program for the specific day or for all days at once?
+
+        #day of the week that this program is being run for
+        self.day = day
+            
+        self.sheet = sheet
+
+        self.row = self.sheet.getRow(row,'xxx.smth')
+
+        self.name = self.row[0]
+        self.car = self.row[1]
+        self.distance = self.row[2]
+
+        #critical need for days of the week
+        self.mon_crit = self.row[3]
+        self.tue_crit = self.row[4]
+        self.wed_crit = self.row[5]
+        self.thur_crit = self.row[6]
+        self.fri_crit = self.row[7]
+        
+        self.sports = self.row[8]
+        
+        self.carpoolUnder = self.row[9]
+        self.carpoolSenior = self.row[10]
+
+        #first period and last period free
+        self.fpFree = self.row[11]
+        self.lpFree = self.row[12]
+
+        #boolean
+        self.parallel = self.row[13]
 
         #the amount of days it has been since the student has been able to park on campus - adjust score based on this variable in generateScore()
         self.spot_since = 0
