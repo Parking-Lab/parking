2fdde0e4278d2056c892e0a8d6a347845cf8336d
ReidDye <40401995+reid23@users.noreply.github.com>
2022-05-17 22:44:13 -0700

    got l1 distances, in test file

---
+++ zipcodeprocessing.py
@@ -0,0 +1,42 @@
+import pandas as pd
+import numpy as np
+
+
+df = pd.read_csv('~/Documents/zipCodes.txt') #read data
+
+print(df.head()) #show stuff
+
+
+df = df[df['ZIP']//10000 == 9] #select only california zip codes
+
+school = np.array([df[df['ZIP'] == 94010]['LAT'],
+                   df[df['ZIP'] == 94010]['LONG']])
+        
+
+school = np.array([school[0][0], school[1][0]])
+coords = df[['LAT', 'LONG']].to_numpy()
+
+dists = coords-school
+dist_abs = np.absolute(dists)
+print(np.array(list(map(np.sum, dist_abs))))
+
+dists = dict(
+            zip
+            (
+                df['ZIP'].to_numpy(), 
+
+                np.array
+                (
+                    list
+                    (
+                        map
+                        (
+                            np.sum, 
+                            dist_abs
+                        )
+                    )
+                )
+            )
+        )
+
+print(dists)
