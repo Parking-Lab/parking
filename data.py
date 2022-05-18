06526c83f7a91502701870f6a7dcbb63f2db7cf6
Alison Soong <alisonsoong@gmail.com>
2022-05-17 23:30:30 -0700

    output zipcode instead of time

--- data.py
+++ data.py
@@ -10,9 +10,9 @@ Encapsulation goes brr
 NOTE from Alison: 
 - other than running the program, there are a few steps still needed by the person managing this program (so 12th grade presidents? idk I forgot who would be running this)
 - on the weekly form, we may want to indicate for what week the weekly form is for (like dates or smth)
-- after the program finishes, the WEEKLY form needs all of its responses cleared (I can’t seem to find a way to do this programmatically)
+- after the program finishes, the WEEKLY form needs all of its responses cleared (I can't seem to find a way to do this programmatically)
     - (navigate to edit form, responses tab, three dots, delete all responses is the bottom most option)
-    - this will prevent people from editing forms that the program can no longer see (currently, the ‘cleaning up’ part of the code completely gets rid of those earlier forms… so might as well encourage people to submit a new form after every week)
+    - this will prevent people from editing forms that the program can no longer see (currently, the 'cleaning up' part of the code completely gets rid of those earlier forms… so might as well encourage people to submit a new form after every week)
 - it is completely okay if the base info form responses are accidentally deleted! It would just prevent people from editing their forms, but they can just submit a new form because our program cleans up the information by only keeping the most recent entries
 '''
 
@@ -28,7 +28,7 @@ class Data:
         self.key = [
                         'Name',
                         'Car Size',
-                        'Distance (time)',
+                        'Distance (zipcode)',
                         'Monday Critical Access',
                         'Tuesday Critical Access',
                         'Wednesday Critical Access',
@@ -70,7 +70,6 @@ class Data:
             
             # 'Distance (time)', 
             zipcode = self.baseInfo.getAddress(name)
-            time = 0 # TODO: integrate with actual calculations
             formattedRow.append(zipcode)
             
             # 'Monday Critical Access',
