440b30ed17c50c1cc014eb928bc807de08078bf0
sun-god007 <72593495+sun-god007@users.noreply.github.com>
2022-05-22 19:40:08 -0700

    updated sorting

--- Student.py
+++ Student.py
@@ -47,8 +47,9 @@ class Student:
             
         self.data = Data()
         self.data = self.data.getFormattedInfo()
-
-        print(self.data)
+        
+##        print('new')
+##        print(self.data)
         
         self.row = self.data[row]
 
@@ -80,6 +81,9 @@ class Student:
 
         #boolean
         self.parallel = self.row[17]
+
+        print('len')
+        print(len(self.row))
 
         #the amount of days it has been since the student has been able to park on campus - adjust score based on this variable in generateScore()
         self.spot_since = 0
@@ -183,16 +187,21 @@ class Student:
         return Student.DISTANCES[zipcode]*1.5
 
 def main():
+    
     student = Student(0,'Monday',2,2)
     student.generateScore()
 
-    student2 = Student(1,'Monday',2,2)
-    student2.generateScore()
+    sheet_length = len(student.data)
+    print(sheet_length)
 
-    student3 = Student(2,'Monday',2,2)
-    student3.generateScore()
+    print('row')
+    print(student.row)
     
-    
+##
+##    student2 = Student(1,'Monday',2,2)
+##    student2.generateScore()
+##
+##    student3 = Student(2,'Monday',2,2)
+##    student3.generateScore()
+
 if __name__ == '__main__': main()
-    
-
