14203c582778caa90517fa6ffe58b8739b07efe2
ReidDye <40401995+reid23@users.noreply.github.com>
2022-05-24 10:30:46 -0700

    sdal;fjsadlk;f

--- Sorter.py
+++ Sorter.py
@@ -7,6 +7,11 @@ import bisect
 import pandas as pd
 from Student import Student
 class Sorter:
+    MAX_REG = 20
+    MAX_SML = 2
+    MAX_PAR = 4
+    MAX_CAMPUS = MAX_REG + MAX_SML + MAX_PAR
+    DAYS = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'] #augh why people why
     def __init__(self, students: 'list[Student]') -> 'None':
         """constructor for Sorter object. sorts students upon intialization.
 
@@ -14,17 +19,18 @@ class Sorter:
             students (list[students]): a list of all students to be sorted
         """
         self.students = dict(zip(students, [[0 for _ in range(5)] for _ in range(len(students))]))
-        self.students = pd.df(self.students, orient = 'index', columns = [0,1,2,3,4])
+        self.students = pd.df(self.students, orient = 'index', columns = Sorter.DAYS)
+        self.students.insert(0, 'name', list(map(lambda x: x.getName(), self._getStudentList())))
         print(self.students.head())
         
-        self._sort()
+        self._addScores()
+    def _getStudentList(self):
+        return list(self.students.index.values())
 
-    def _sort(self):
-        students = list(self.students.index.values())
-        for day in range(5):
-            students.sort(key = lambda x: x.generateScore(day))
-            for rank, s in enumerate(students):
-                self.students[s][day] = rank
+    def _addScores(self): #basically, get all the students, sort them separately for each day, then put the rankings into the df
+        for s in self._getStudentList():
+            for day in Sorter.DAYS:
+                self.students[s][day] = s.generateScores(day)
 
     def add(self, *student: 'Student') -> 'None':
         """Adds `student` to the list of students, and places them in the correct order
@@ -34,14 +40,14 @@ class Sorter:
         """
         for s in student:
             self.students[s] = list(range(5))
-        self._sort()
+        self._addScores()
 
-    def __getitem__(self, day: int, idx: 'int|str|Student' = 0) -> 'Student|int':
+    def __getitem__(self, idx: 'int|str|Student', day: int = None) -> 'Student|int':
         """gets an item, using subscript notation like a list. Takes an int (rank), str (student's name), or Student (actual equivalent student object).
 
         Args:
-            day (int): int in [0, 4] describing the day of the week requested.
-            idx (int|str|Student, optional): if int, returns student in that rank (0 is best, can be negative, return Student). If str, returns rank of student with that name (int). If Student, returns rank of that Student (int). Defaults to -1.
+            day (int): int in [0, 4] describing the day of the week requested. Defaults to None, returning list of all days.
+            idx (int|str|Student): if int, returns student in that rank (0 is best, can be negative, return Student). If str, returns rank of student with that name (int). If Student, returns rank of that Student (int).
 
         Raises:
             ValueError: if the str name or Student passed does not exist in the Sorter
@@ -51,23 +57,15 @@ class Sorter:
         Returns:
             Student|int : the rank or student requested
         """
+        if day == None:
+            return [self[d, idx] for d in range(5)]
         if isinstance(idx, int):
-            return self.students.loc[self.students[day]==idx]
+            return self.students.sort_values(Sorter.DAYS[day], axis = 'columns', ascending = False).index.values()[idx]
         if isinstance(idx, str):
-            return self.students[list(map(lambda x: x.name, self.students)).index(idx)][day]
+            return self.students.sort_values(Sorter.DAYS[day], axis = 'columns', ascending = False)['name'].to_list().index(idx)
         if isinstance(idx, Student):
-            return self.students[self.students.index(idx)][day]
+            return self.students.sort_values(Sorter.DAYS[day], axis = 'columns', ascending = False).iloc(idx, Sorter.DAYS[day])
         raise TypeError(f'Expected idx of type int|str|Student but received {type(idx)}.')
-    def pop(self, day: int) -> 'tuple[Student, int]':
-        """returns (and deletes) top ranked student, like pop function in a standard queue.
-
-        Args:
-            day (int): the day of the week you want the rankings from, in range [0, 4]
-
-        Returns:
-            tuple[Student, int]: tuple in the format (Student (Student object), rank (int))
-        """
-        pass
 
     def delete(self, day: int, student: 'int|str|Student') -> 'None':
         """deletes the student at the rank, with the name, or equal to the Student.
@@ -81,7 +79,13 @@ class Sorter:
             day (int): the day of the week you want the rankings to reference, in range [0, 4]
             student (int|str|Student): the student to delete. Specify rank (can be negative), name, or an identical Student object.
         """
-        pass
+        if isinstance(student, int): # it's a rank
+            self.students.drop(#        sort by rank on day                 in descending order        get index at rank
+                self.students.sort_values(Sorter.DAYS[day], axis = 'columns', ascending = False).index.values()[student])
+        if isinstance(student, str): # it's a name
+            self.students.drop(self.students[self.students['name'] == student]) #drop all columns where the name is student
+        if isinstance(student, Student): #it's a student object
+            self.students.drop(student) #just drop the column, easy
 
     def __add__(self, other: 'Sorter') -> 'Sorter':
         """merges this an the other Sorter together, and returns the result. Does not modify either Sorter.
@@ -95,15 +99,9 @@ class Sorter:
         Returns:
             Sorter: the merge result
         """
-        pass
-
-    def _insert(self, student: 'Student') -> 'None':
-        """Uses binary search to insert a Student into the correct position in the sorted list.
-
-        Args:
-            student (Student): the student to insert into the Sorter.
-        """
-        pass
+        if not isinstance(other, Sorter):
+            raise TypeError(f'Expected `other` of type Sorter but received {type(other)}')
+        return Sorter(other._getStudentList()+self._getStudentList())
 
     def getAssignments(self) -> 'tuple[dict[str, list[Student]]]':
         """gets the final assignments for the week's parking
@@ -111,7 +109,27 @@ class Sorter:
         Returns:
             tuple[dict[str, list[Student]]]: list of a dict for each day containing {parking zone: list of student objects}
         """
-        pass
+        #for each day
+            #get order of students
+            #make and append dict of zones based on rank
+        output = [dict(zip(['REG', 'SML', 'PAR'], 
+                           [[],    [],    []])) for _ in range(5)]
+        for students in self:
+            for day, student in enumerate(students):
+                if student.canParallelPark() and len(output[day]['PAR'])<Sorter.MAX_PAR:
+                    output[day]['PAR'].append(student)
+                elif student.hasSmallCar() and len(output[day]['SML'])<Sorter.MAX_SML:
+                    output[day]['SML'].append(student)
+                elif len(output[day]['REG'])<Sorter.MAX_REG:
+                    output[day]['REG'].append(student)
+                if student.canParallelPark() and not student.hasSmallCar() and len(output[day]['PAR'])==Sorter.MAX_PAR and len(output[day]['SML'])<Sorter.MAX_SML and len(output[day]['REG'])==Sorter.MAX_REG and True in list(map(lambda s: s.canParallelPark() and s.hasSmallCar(), output[day]['PAR'])):
+                    pass #panic! there's a problem!
+                    # problem: if someone can park in par and small, they get put in par, but if there's someone 
+                    # else later who can park in par but not sml, and par and reg is full, but sml is not, they'd 
+                    # get put in barts even when they can park on campus
+            
+
+
 
     def __iter__(self) -> 'Sorter':
         """initialization method for for loops.
@@ -119,7 +137,8 @@ class Sorter:
         Returns:
             Sorter: this Sorter object, initialized for iteration
         """
-        pass
+        self.n = -1
+        return self
 
     def __next__(self) -> 'Student':
         """gets the next element in self.
@@ -127,7 +146,11 @@ class Sorter:
         Returns:
             Student: the next ranked student.
         """
-        pass
+        if self.n < len(self.students)-1:
+            self.n+=1
+            return self[self.n]
+        else:
+            raise StopIteration
 
 
 def main():
