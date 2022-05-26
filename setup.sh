d3ed1e6a2c090cc65d91c41a3e9f46d60140ae48
ReidDye <40401995+reid23@users.noreply.github.com>
2022-05-25 18:18:06 -0700

    fixed python pathing problem

--- setup.sh
+++ setup.sh
@@ -10,7 +10,9 @@ mkdir ~/.config/gspread
 mv Downloads/parking_service_account.json ~/.config/gspread/service_account.json
 
 #make student list
-python parking/listStudents.py
+cd parking
+python listStudents.py
+cd
 
 #make an alias in bashrc to the runner file
 echo  'alias assignparking="python ~/parking/Sorter.py"' >> ~/.zshrc
