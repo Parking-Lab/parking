67bffbfffc656b2a6155adfa5f89f4e5d563b0f8
ReidDye <40401995+reid23@users.noreply.github.com>
2022-05-25 17:13:51 -0700

    setup should be finished

--- setup.sh
+++ setup.sh
@@ -11,3 +11,16 @@ pip install numpy pandas gspread
 
 #make student list
 python listStudents.py
+
+#stuff with service acct
+mkdir ~/.config/gspread 
+cp Downloads/parking_service_account.json ~/.config/gspread/service_account.json
+
+#make an alias in bashrc to the runner file
+echo  'alias assignparking="python ~/parking/Sorter.py"' >> ~/.zshrc
+echo  'alias assignparking="python ~/parking/Sorter.py"' >> ~/.bashrc #backwards compatability!
+
+source ~/.bashrc #get those updates
+
+#yay! done!
+echo 'Setup complete!'
