d1253700a7071e982c79c2f1dc81bb2d5fa26417
ReidDye <40401995+reid23@users.noreply.github.com>
2022-05-25 17:56:30 -0700

    fixed some bugs (tested on my computer)

--- setup.sh
+++ setup.sh
@@ -2,25 +2,21 @@
 python -m ensurepip --default-pip
 python -m pip install --upgrade pip
 
-#then install virtualenv and make a new env
-pip install --user virtualenv
-python -m virtualenv parking-env
-
 #install stuff
 pip install numpy pandas gspread
 
 #make student list
-python listStudents.py
+python parking/listStudents.py
 
 #stuff with service acct
 mkdir ~/.config/gspread 
 cp Downloads/parking_service_account.json ~/.config/gspread/service_account.json
 
 #make an alias in bashrc to the runner file
-echo  'alias assignparking="source parking-env/bin/activate; python ~/parking/Sorter.py; deactivate"' >> ~/.zshrc
-echo  'alias assignparking="source parking-env/bin/activate; python ~/parking/Sorter.py; deactivate"' >> ~/.bashrc #backwards compatability!
+echo  'alias assignparking="python ~/parking/Sorter.py"' >> ~/.zshrc
+echo  'alias assignparking="python ~/parking/Sorter.py"' >> ~/.bashrc #backwards compatability!
 
-source ~/.bashrc #get those updates
+source ~/.zshrc #get those updates
 
 #yay! done!
 echo 'Setup complete!'
